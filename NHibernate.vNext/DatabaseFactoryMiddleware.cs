aab38ae46ecc7e746f05a08654d247a1f31d9b05
Allan Clempe <allan.clempe@gmail.com>
2015-11-19 21:15:15 -0200

    Fixes #1

---
+++ NHibernate.vNext/DatabaseFactoryMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+
+namespace NHibernate.vNext
+{
+    public class DatabaseFactoryMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IDatabaseFactory _databaseFactory;
+
+        public DatabaseFactoryMiddleware(RequestDelegate next, IDatabaseFactory databaseFactory)
+        {
+            _next = next;
+            _databaseFactory = databaseFactory;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var request = _databaseFactory.BeginRequest();
+            await _next.Invoke(context);
+            request.Finish();
+        }
+
+    }
+}
